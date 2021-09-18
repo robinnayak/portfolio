c9b2eba23f1923448f98b419ad7c038d855869f3
robinnayak <robinnayak86@gmail.com>
2021-09-19 01:14:34 +0545

    test material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import { Home } from "@material-ui/icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      hello world
+      <Button color="primary" >Click me .</Button>
+      <Home color = "secondary" /> 
+      </div>
   );
 }
 
